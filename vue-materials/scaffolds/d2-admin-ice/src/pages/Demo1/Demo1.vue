aa15676a7e75972ccafc378d15d0ad8a5ffa8d91
liyang <1711467488@qq.com>
2018-07-26 17:26:24 +0800

    增加注释

--- vue-materials/scaffolds/d2-admin-ice/src/pages/Demo1/Demo1.vue
+++ vue-materials/scaffolds/d2-admin-ice/src/pages/Demo1/Demo1.vue
@@ -1,8 +1,6 @@
 <template>
   <d2-container>
-    <template slot="header">
-      WELCOME
-    </template>
+    <template slot="header">WELCOME</template>
     <route-info/>
     <template slot="footer">
       <footer-link/>
@@ -14,6 +12,8 @@
 import RouteInfo from './componnets/RouteInfo'
 import FooterLink from './componnets/FooterLink'
 export default {
+  // 如果需要缓存页面
+  // name 字段需要设置为和本页路由 name 字段一致
   name: 'demo1',
   components: {
     RouteInfo,
