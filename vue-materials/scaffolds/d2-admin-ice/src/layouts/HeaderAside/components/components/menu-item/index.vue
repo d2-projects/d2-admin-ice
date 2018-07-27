d7b00b07b5ef4116360002cd900c6b9109780f1c
liyang <1711467488@qq.com>
2018-07-27 09:40:18 +0800

    菜单默认值

--- vue-materials/scaffolds/d2-admin-ice/src/layouts/HeaderAside/components/components/menu-item/index.vue
+++ vue-materials/scaffolds/d2-admin-ice/src/layouts/HeaderAside/components/components/menu-item/index.vue
@@ -1,13 +1,12 @@
 <template>
   <el-menu-item :index="menu.path || uniqueid">
     <i :class="`fa fa-${menu.icon || 'file-o'}`"></i>
-    <span slot="title">{{menu.title}}</span>
+    <span slot="title">{{menu.title || '未命名菜单'}}</span>
   </el-menu-item>
 </template>
 
 <script>
 import uniqueid from 'lodash.uniqueid'
-
 export default {
   name: 'd2-layout-header-aside-menu-item',
   props: {
