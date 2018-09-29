1ec21026168f60fda40b0edaa7d6cea8b33a8df8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-29 08:55:55 +0000

    Deploy d2-projects/d2-admin-ice to github.com/d2-projects/d2-admin-ice.git:gh-pages

--- js/chunk-6c87.721ca7d4.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-6c87"],{"01bf":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("i",{staticClass:"fa",class:"fa-"+e.name,attrs:{"aria-hidden":"true"}})},c=[],i=(t("5ccc"),{name:"d2-icon",props:{name:{type:String,required:!1,default:"font-awesome"}}}),s=i,r=t("2877"),u=Object(r["a"])(s,a,c,!1,null,null,null);u.options.__file="index.vue";n["default"]=u.exports},"5ccc":function(e,n,t){}}]);
-//# sourceMappingURL=chunk-6c87.721ca7d4.js.map
